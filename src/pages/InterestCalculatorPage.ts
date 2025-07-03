aad8e1e75393e48701670f178f18695676abea76
Mark Guhathakurta-Halliday <133343189+WebTestMan@users.noreply.github.com>
2025-07-03 11:22:34 +0100

    added locators

--- src/pages/InterestCalculatorPage.ts
+++ src/pages/InterestCalculatorPage.ts
@@ -8,18 +8,26 @@ export class InterestCalculatorPage {
   readonly yearlyInterestBtn: Locator;
   readonly principalAmountField: Locator;
   readonly interestAmountDropdown: Locator;
+  readonly calculateInterestBtn: Locator;
+  readonly consentCheckBox: Locator;
   readonly calculatedInterest: Locator;
   readonly totalAmount: Locator;
 
   constructor(page: Page) {
     this.calculatorPageTitle = page.locator("h1");
-    this.dailyInterestBtn = page.getByRole();
-    this.monthlyInterestBtn = page.getByRole();
-    this.yearlyInterestBtn = page.getByRole();
-    this.principalAmountField = getByRole("slider", {
+    this.dailyInterestBtn = page.getByRole("link", { name: "Daily" });
+    this.monthlyInterestBtn = page.getByRole("link", { name: "Monthly" });
+    this.yearlyInterestBtn = page.getByRole("link", { name: "Yearly" });
+    this.principalAmountField = page.getByRole("slider", {
       name: "Principal Amount:",
     });
-    this.interestAmountDropdown = page.getByRole();
+    this.interestAmountDropdown = page.getByRole("button", {
+      name: "Select Interest Rate",
+    });
+    this.calculateInterestBtn = page.getByRole("button", { name: "Calculate" });
+    this.consentCheckBox = page.getByRole("checkbox", {
+      name: "Please accept this mandatory",
+    });
     this.calculatedInterest = page.getAttribute();
     this.totalAmount = page.locator("");
   }
@@ -40,16 +48,16 @@ export class InterestCalculatorPage {
 }
 
 test("test", async ({ page }) => {
-  await page.getByRole("textbox", { name: "Email" }).click();
-  await page
-    .getByRole("textbox", { name: "Email" })
-    .fill("markdavidhalliday@gmail.com");
-  await page.getByRole("textbox", { name: "Password" }).click();
-  await page
-    .getByRole("textbox", { name: "Password" })
-    .fill("tqx1utu0VMA7jfp!hqp");
-  await page.getByRole("button", { name: "Log in" }).click();
-  await expect(page.locator("h1")).toContainText("Interest Calculator");
+  // await page.getByRole("textbox", { name: "Email" }).click();
+  // await page
+  //   .getByRole("textbox", { name: "Email" })
+  //   .fill("markdavidhalliday@gmail.com");
+  // await page.getByRole("textbox", { name: "Password" }).click();
+  // await page
+  //   .getByRole("textbox", { name: "Password" })
+  //   .fill("tqx1utu0VMA7jfp!hqp");
+  // await page.getByRole("button", { name: "Log in" }).click();
+  // await expect(page.locator("h1")).toContainText("Interest Calculator");
   await expect(page.getByRole("main")).toContainText("Principal Amount:");
   await expect(page.getByRole("link", { name: "Daily" })).toBeVisible();
   await expect(page.getByRole("link", { name: "Monthly" })).toBeVisible();
@@ -95,7 +103,4 @@ test("test", async ({ page }) => {
   await expect(
     page.getByRole("heading", { name: "Please enter your login" })
   ).toBeVisible();
-  await expect(page.getByRole("textbox", { name: "Email" })).toBeVisible();
-  await expect(page.getByRole("textbox", { name: "Password" })).toBeVisible();
-  await expect(page.getByRole("button", { name: "Log in" })).toBeVisible();
 });
